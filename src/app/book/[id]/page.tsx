8c95172e69ec45e84429bd45e6c3f015106d7f02
J-mung <xorb2298@gmail.com>
2026-02-22 02:21:06 +0900

    서버 액션 및 결과 자동 갱신(리뷰 작성 및 삭제)

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { BookData } from "@/types";
+import ReviewEditor from "@/components/review-editor";
+import ReviewItem from "@/components/review-item";
+import { BookData, ReviewData } from "@/types";
 import style from "./page.module.css";
 
 /**
@@ -15,6 +17,54 @@ export async function generateStaticParams() {
   return books.map((_book) => ({
     id: String(_book.id),
   }));
+}
+
+async function BookDetail({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/book/${bookId}`,
+    {
+      cache: "force-cache",
+    },
+  );
+  if (!response.ok) throw new Error(response.statusText);
+
+  const book: BookData = await response.json();
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+  return (
+    <section>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+async function ReviewList({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } },
+  );
+  if (!response.ok) throw new Error(response.statusText);
+
+  const reviews: ReviewData[] = await response.json();
+
+  return (
+    <section>
+      {reviews.map((_review) => (
+        <ReviewItem key={`review-item-${_review.id}`} {..._review} />
+      ))}
+    </section>
+  );
 }
 
 export default async function Page({
@@ -37,18 +87,9 @@ export default async function Page({
 
   return (
     <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
-      </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
-      <div className={style.description}>{description}</div>
+      <BookDetail bookId={id} />
+      <ReviewEditor bookId={id} />
+      <ReviewList bookId={id} />
     </div>
   );
 }
